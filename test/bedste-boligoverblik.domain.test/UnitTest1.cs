b8324a2cdccf62352438da434053bd73a1556db9
Lars Larsen <lars@k7c.dk>
2021-03-30 10:22:06 +0200

    tilføjet test projekt for domain

---
+++ test/bedste-boligoverblik.domain.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace bedste_boligoverblik.domain.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
