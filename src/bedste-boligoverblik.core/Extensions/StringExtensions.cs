959625888224305742b058cfcd2a870fbf50bbcf
Lars Larsen <lars@k7c.dk>
2021-03-30 15:31:23 +0200

    fixed fejlende test

--- src/bedste-boligoverblik.core/Extensions/StringExtensions.cs
+++ src/bedste-boligoverblik.core/Extensions/StringExtensions.cs
@@ -10,22 +10,7 @@ namespace bedste_boligoverblik.core.Extensions
 
         public static decimal ToDecimal(this string s, int noOfDecimals = 2)
         {
-            var sValue = "0";
-
-            if (s != null)
-            {
-
-                var indexOf = s.IndexOf(".", StringComparison.CurrentCulture);
-                var totalWidth = indexOf + 1 + noOfDecimals;
-
-                sValue = s.Length > totalWidth && indexOf > 0
-                    ? s.Substring(0, totalWidth)
-                    : s.PadRight(totalWidth, '0');
-
-               sValue = sValue.Replace(".", ",");
-            }
-
-            var result = decimal.Parse(sValue);
+            var result = s == null ? 0 : decimal.Parse(s.Replace(".", ","));
             return Math.Round(result, noOfDecimals);
         }
 
