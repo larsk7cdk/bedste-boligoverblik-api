5eefb4720fb9ab5b889572a8bb9f1a5d9eb3ec87
Lars Larsen <lars@k7c.dk>
2021-05-08 09:36:52 +0200

    tilføjet b2c konf

--- src/bedste-boligoverblik.api/Startup.cs
+++ src/bedste-boligoverblik.api/Startup.cs
@@ -3,10 +3,12 @@ using bedste_boligoverblik.core.Configuration;
 using bedste_boligoverblik.domain.Configuration;
 using bedste_boligoverblik.proxy.Configuration;
 using bedste_boligoverblik.storage.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
 
 namespace bedste_boligoverblik.api
 {
@@ -22,6 +24,16 @@ namespace bedste_boligoverblik.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adds Microsoft Identity platform (AAD v2.0) support to protect this Api
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(options =>
+                    {
+                        Configuration.Bind("AzureAdB2C", options);
+                        
+                        options.TokenValidationParameters.NameClaimType = "name";
+                    },
+                    options => { Configuration.Bind("AzureAdB2C", options); });
+
             ConfigureCore.Configure(services);
             ConfigureStorage.Configure(services, Configuration);
             ConfigureDomain.Configure(services);
